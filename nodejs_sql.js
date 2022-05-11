325c7b11a2441dd29db168d62233647331014e49
GoRuFu <golflovex4@gmail.com>
2022-05-11 17:19:18 +0700

    INSERT Finnish

--- nodejs_sql.js
+++ nodejs_sql.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
  
 const app = express()
-const port = process.env.PORT || 3360;
+const port = process.env.PORT || 5000;
  
 app.use(bodyParser.json())
- 
+app.set('view engine','ejs')
+
+var obj = {}
+
 //MySQL Connect phpMyAdmin
 const pool = mysql.createPool({
     connectionLimit : 10,
@@ -16,9 +19,14 @@ const pool = mysql.createPool({
     host : 'localhost', //www.google.com/sql or Server IP Address
     user : 'root',
     password : '',
-    database : 'Final_ex.sql' //Connect Database from beers.sql (Import to phpMyAdmin)
+    database : 'Final_Ex' //Connect Database from Final_Ex.sql (Import to phpMyAdmin)
 })
- 
+
+app.get('/addlottery',(req, res) => {   
+    res.render('addlottery')
+})
+
+
 //GET (เรียกข้อมูลขึ้นมาดู) | POST (ส่งข้อมูลหน้า Website กลับเข้ามา)
 //GET All Lottery (Lottery.sql)
 app.get('',(req, res) => {
@@ -26,20 +34,55 @@ app.get('',(req, res) => {
     pool.getConnection((err, connection) => {  //err คือ connect ไม่ได้ or connection คือ connect ได้ บรรทัดที่ 13-20
         if(err) throw err
         console.log("connected id : ?" ,connection.threadId) //ให้ print บอกว่า Connect ได้ไหม
-        //console.log(`connected id : ${connection.threadId}`) //ต้องใช้ ` อยู่ตรงที่เปลี่ยนภาษา ใช้ได้ทั้ง 2 แบบ
+        
          
         connection.query('SELECT * FROM lottery', (err, rows) => { 
             connection.release();
-            if(!err){ //ถ้าไม่ error จะใส่ในตัวแปร rows
-                console.log(rows)
+            if(!err){ //ถ้าไม่ error
+
+                obj = {lottery : rows , Error : err} //Model data 
+                
+                res.render('index', obj) //ส่งไปที่หน้า index
+
             } else {
                 console.log(err)
             }
          }) 
     })
 })
- 
+
+
+app.use(bodyParser.urlencoded({extended: false})) 
+//สร้าง Path ของเว็บไซต์ additem
+app.post('/addlottery',(req, res) => {
+    pool.getConnection((err, connection) => { //pool.getConnection สำหรับใช้เชื่อมต่อกับ Database 
+        if(err) throw err
+            const params = req.body
+
+                //Check 
+                pool.getConnection((err, connection2) => {
+                    connection2.query(`SELECT COUNT(id) AS count FROM lottery WHERE id = ${params.id}`, (err, rows) => {
+                        if(!rows?.[0].count){
+                            connection.query('INSERT INTO Lottery SET ?', params, (err, rows) => {
+                                connection.release()
+                                if(!err){
+                                    obj = {Error:err, mesg : `Success adding data ${params.Number}`}
+                                    res.render('addlottery', obj)
+                                }else {
+                                    console.log(err)
+                                    }
+                                })           
+                        } else {
+                            obj = {Error:err, mesg : `Can not adding data ${params.Number}`}
+                            res.render('addlottery', obj)
+                            }
+                        })
+                    })
+                })
+            })
+
 app.listen(port, () => 
     console.log("listen on port : ?", port)
     )
+
     
